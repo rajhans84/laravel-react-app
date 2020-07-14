5de8a0c6e34b8c129ed4cf648de2ae5fe7f737e0
rajhans84 <rajhans.nz@gmail.com>
2020-07-14 22:06:29 +1200

    Delete DeleteOrg.js
    Duplicate file

--- resources/js/components/DeleteOrg.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-import Delete from "./Delete";
-
-export default function DeleteOrg() {
-    let { id } = useParams();
-    return <Delete url={`/organisations/${id}`} />;
-}
